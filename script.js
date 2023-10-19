c2b48d2648a9b51b72b7f7f2fe3905abd0d1d4fa
Perg01 <jsatshs@gmail.com>
2023-10-18 21:55:45 -0700

    Div color won't change

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 let row = 16;
 let column = 16;
 const grid = document.querySelector('.container');
+let colClicks = document.getElementsByClassName('colBoxes');
+let rowClicks = document.getElementsByClassName('rowBoxes');
+
+// function color(event) {
+//     event.target.style.backgroundColor = 'black';
+// }
 
 function appendBoxes() {
     for (let i = 0; i < column; i++) {
-        const colDivs = document.createElement('div');
+        let colDivs = document.createElement('div');
         colDivs.classList.add('colBoxes');
         for (let j = 0; j < row; j++) {
-            const rowDivs = document.createElement('div');
+            let rowDivs = document.createElement('div');
             rowDivs.classList.add('rowBoxes');
             colDivs.appendChild(rowDivs);
         }
         grid.appendChild(colDivs);
     }
-
 }
 appendBoxes();
+
+colClicks.forEach(clk => clk.addEventListener('mouseover', color));
+// rowClicks.forEach(clk => clk.addEventListener('click', color));
+// grid.addEventListener('mousedown', event =>)
+
