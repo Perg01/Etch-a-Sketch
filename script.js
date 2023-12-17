c083a9cc6a8872138b2dea21904e602271d76a4b
Jaspreet Singh <134102107+Perg01@users.noreply.github.com>
2023-12-16 18:51:23 -0800

    Added a button that changes the size of the grid and reverts the color of each squares to the original color

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 let row = 16;
 let column = 16;
 const grid = document.querySelector('.container');
-
+let sizeBtn = document.querySelector('.grid-btn');
 
 function color(e) {
     e.target.setAttribute('style', 'background-color: black;');
-    // e.target.style.cssText = "background-color: black;";
 }
 
-function appendBoxes() {
+function gridSizing() {
+    let size = prompt("Set a custom grid size(1 <= size <= 100):", "16");
+    if (size >= 1 && size <= 100) {
+        resetColor();
+        resetGrid();
+        row = size;
+        column = size;
+        appendBoxes(row, column);
+    }
+    else {
+        alert("Sorry, grid size must be between 1 and 100. Please try again.")
+    }
+}
+
+function resetColor() {
+    let selectAllDivs = document.querySelectorAll('.rowBoxes');
+    selectAllDivs.forEach(div => {
+        div.setAttribute('style', 'background-color: white;');
+    });
+}
+
+function resetGrid() {
+    let selectAllDivs = document.querySelectorAll('.rowBoxes');
+    selectAllDivs.forEach(div => {
+        div.classList.remove('rowBoxes');
+    });
+}
+
+function appendBoxes(row, column) {
     for (let i = 0; i < column; i++) {
         let colDivs = document.createElement('div');
         colDivs.classList.add('colBoxes');
@@ -21,6 +48,8 @@ function appendBoxes() {
         grid.appendChild(colDivs);
     }
 }
+
+sizeBtn.addEventListener('click', gridSizing);
 
 appendBoxes();
 
